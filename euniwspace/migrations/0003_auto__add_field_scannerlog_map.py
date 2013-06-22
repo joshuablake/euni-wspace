4d45c6ac15f526f33961ab7cd09cb41c701f76e5
Joshua Blake <joshbblake@gmail.com>
2013-06-22 12:43:49 +0100

    Track map where signature added

---
+++ euniwspace/migrations/0003_auto__add_field_scannerlog_map.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'ScannerLog.map'
+        db.add_column('euniwspace_scannerlog', 'map',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['Map.Map']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'ScannerLog.map'
+        db.delete_column('euniwspace_scannerlog', 'map_id')
+
+
+    models = {
+        'Map.map': {
+            'Meta': {'object_name': 'Map'},
+            'explicitperms': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'root': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'root'", 'to': "orm['Map.System']"})
+        },
+        'Map.signaturetype': {
+            'Meta': {'object_name': 'SignatureType'},
+            'escalatable': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'longname': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'shortname': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
+            'sleeprsite': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'Map.system': {
+            'Meta': {'object_name': 'System', '_ormbases': ['core.SystemData']},
+            'first_visited': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'info': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'last_visited': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'lastscanned': ('django.db.models.fields.DateTimeField', [], {}),
+            'npckills': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'occupied': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'podkills': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'shipkills': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'sysclass': ('django.db.models.fields.IntegerField', [], {}),
+            'systemdata_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.SystemData']", 'unique': 'True', 'primary_key': 'True'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.constellation': {
+            'Meta': {'object_name': 'Constellation', 'db_table': "'mapConstellations'", 'managed': 'False'},
+            'id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_column': "'constellationID'"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_column': "'constellationName'"}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'constellations'", 'db_column': "'regionID'", 'to': "orm['core.Region']"}),
+            'x': ('django.db.models.fields.FloatField', [], {}),
+            'y': ('django.db.models.fields.FloatField', [], {}),
+            'z': ('django.db.models.fields.FloatField', [], {})
+        },
+        'core.region': {
+            'Meta': {'object_name': 'Region', 'db_table': "'mapRegions'", 'managed': 'False'},
+            'id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_column': "'regionID'"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_column': "'regionName'"}),
+            'x': ('django.db.models.fields.FloatField', [], {}),
+            'y': ('django.db.models.fields.FloatField', [], {}),
+            'z': ('django.db.models.fields.FloatField', [], {})
+        },
+        'core.systemdata': {
+            'Meta': {'object_name': 'SystemData', 'db_table': "'mapSolarSystems'", 'managed': 'False'},
+            'constellation': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'systems'", 'db_column': "'constellationID'", 'to': "orm['core.Constellation']"}),
+            'id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_column': "'solarSystemID'"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_column': "'solarSystemName'"}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'systems'", 'db_column': "'regionID'", 'to': "orm['core.Region']"}),
+            'security': ('django.db.models.fields.FloatField', [], {}),
+            'x': ('django.db.models.fields.FloatField', [], {}),
+            'y': ('django.db.models.fields.FloatField', [], {}),
+            'z': ('django.db.models.fields.FloatField', [], {})
+        },
+        'euniwspace.scannerlog': {
+            'Meta': {'object_name': 'ScannerLog'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('django.db.models.fields.CharField', [], {'max_length': '65', 'null': 'True'}),
+            'map': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Map.Map']"}),
+            'sig_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Map.SignatureType']", 'null': 'True'}),
+            'sigid': ('django.db.models.fields.CharField', [], {'max_length': '7'}),
+            'strength': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '5', 'decimal_places': '2'}),
+            'system': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Map.System']"}),
+            'time': ('django.db.models.fields.DateTimeField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'euniwspace.viewtime': {
+            'Meta': {'object_name': 'ViewTime'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'time': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '4'}),
+            'view': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['euniwspace']
--- euniwspace/models.py
+++ euniwspace/models.py
@@ -1,6 +1,6 @@
 # E-Uni wspace
 # Copyright (c) 2013 Joshua Blake <joshbblake@gmail.com>
-from Map.models import System, SignatureType
+from Map.models import System, SignatureType, Map
 from Map.signals import signature_update
 from datetime import datetime
 from django.contrib.auth.models import User
@@ -15,6 +15,7 @@ class ScannerLog(models.Model):
     sig_type = models.ForeignKey(SignatureType, null=True)
     info = models.CharField(max_length=65, null=True)
     strength = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    map = models.ForeignKey(Map)
 
 @receiver(signature_update)
 def add_log(**kwargs):
@@ -25,5 +26,4 @@ def add_log(**kwargs):
         signal_strength = None
     ScannerLog(user=kwargs['user'], time=datetime.now(), system=signature.system,
         sigid=signature.sigid, sig_type=signature.sigtype,
-        info=signature.info, strength=signal_strength).save()
-    log.save()
+        info=signature.info, strength=signal_strength, map=kwargs['map']).save()
