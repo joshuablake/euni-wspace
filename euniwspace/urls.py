6d2672dd831fc2b36cf33ae6ac86b9fc6e5f0950
Joshua Blake <joshbblake@gmail.com>
2013-07-04 15:50:11 +0100

    Add XML format
    Format is determined by ending of requested page

--- euniwspace/urls.py
+++ euniwspace/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns
 
-BASE_SCAN_URL = r'^scanners/(?P<auth>[^/]+)/(?P<map_id>\d+)/(?P<from_date>\d{4}-\d{2}-\d{2})/'
+BASE_SCAN_URL = r'^scanners/(?P<auth>[^/]+)/(?P<map_id>\d+)/(?P<from_date>\d{4}-\d{2}-\d{2})'
 
 urlpatterns = patterns('euniwspace.views',
-    (BASE_SCAN_URL + '$', 'CSV_scanners'),
-    (BASE_SCAN_URL + '(?P<to_date>\d{4}-\d{2}-\d{2})/$', 'CSV_scanners')
+    (BASE_SCAN_URL + '\.(?P<format>[a-z]{3})/$', 'scanners'),
+    (BASE_SCAN_URL + '/(?P<to_date>\d{4}-\d{2}-\d{2})\.(?P<format>[a-z]{3})/$', 'scanners')
 )
--- euniwspace/views.py
+++ euniwspace/views.py
@@ -6,28 +6,36 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from euniwspace.models import ScannerLog
 import datetime
+from django.shortcuts import render
 
-def CSV_scanners(request, auth, map_id, from_date, to_date=None):
+def scanners(request, auth, map_id, from_date, format, to_date=None):
     """Take logs between requested dates and display in csv"""
-    if settings.EUNI_AUTH != auth: raise PermissionDenied 
+    if settings.EUNI_AUTH != auth or format not in ('csv', 'xml'):
+        raise PermissionDenied 
     if not to_date:
-        to_date = datetime.date.today()
+        to_date = datetime.datetime.combine(datetime.date.today(),
+                                            datetime.time.max)
     else:
         to_date = datetime.datetime.combine(datetime.datetime.strptime(
                         to_date, '%Y-%m-%d'), datetime.time.max)
         
     from_date = datetime.datetime.combine(datetime.datetime.strptime(
                         from_date, '%Y-%m-%d'), datetime.time.min)
-    response = HttpResponse(mimetype='text/csv')
     logs = ScannerLog.objects.filter(time__range=(from_date, to_date))\
             .filter(map_id=map_id).select_related('user__username',
                 'system__name', 'sig_type__longname')
-    csv = writer(response)
-    csv.writerow(('Scanner', 'Time', 'System', 'Signature ID', 'Type', 'Info',
-                 'Strength'))
-    for log in logs:
-        csv.writerow((log.user.username, log.time.strftime('%Y-%m-%d %H:%M'),
-                     log.system.name, log.sigid, log.sig_type.longname, 
-                     log.info, log.strength))
-    return response
-    
+            
+    if format == 'csv':
+        response = HttpResponse(mimetype='text/csv')
+        csv = writer(response)
+        csv.writerow(('Scanner', 'Time', 'System', 'Signature ID', 'Type', 'Info',
+                     'Strength'))
+        for log in logs:
+            csv.writerow((log.user.username, log.time.strftime('%Y-%m-%d %H:%M'),
+                         log.system.name, log.sigid, log.sig_type.longname, 
+                         log.info, log.strength))
+        return response
+     
+    elif format == 'xml':
+        return render(request, 'scanners.xml', {'logs': logs},
+                      content_type='application/xml')
